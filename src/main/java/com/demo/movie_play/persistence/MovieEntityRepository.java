ccf3e040f7e618aec92d1534308ff77b57d7d943
ancu-98 <jdsebastianb30@hotmail.com>
2026-02-18 20:07:38 -0500

    configure MapStruct with Spring to convert entities to DTOs

--- src/main/java/com/demo/movie_play/persistence/MovieEntityRepository.java
+++ src/main/java/com/demo/movie_play/persistence/MovieEntityRepository.java
@@ -1,4 +1,25 @@
 package com.demo.movie_play.persistence;
 
-public class MovieEntityRepository {
+import com.demo.movie_play.domain.dto.MovieDto;
+import com.demo.movie_play.domain.repository.MovieRepository;
+import com.demo.movie_play.persistence.crud.CrudMovieEntity;
+import com.demo.movie_play.persistence.mapper.MovieMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class MovieEntityRepository implements MovieRepository {
+    private final CrudMovieEntity crudMovieEntity;
+    private final MovieMapper movieMapper;
+
+    public MovieEntityRepository(CrudMovieEntity crudMovieEntity, MovieMapper movieMapper){
+        this.crudMovieEntity = crudMovieEntity;
+        this.movieMapper = movieMapper;
+    }
+
+    @Override
+    public List<MovieDto> getAll() {
+        return this.movieMapper.toDto(this.crudMovieEntity.findAll());
+    }
 }
--- src/main/java/com/demo/movie_play/persistence/mapper/MovieMapper.java
+++ src/main/java/com/demo/movie_play/persistence/mapper/MovieMapper.java
@@ -1,4 +1,20 @@
 package com.demo.movie_play.persistence.mapper;
 
+import com.demo.movie_play.domain.dto.MovieDto;
+import com.demo.movie_play.persistence.entity.MovieEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
 public interface MovieMapper {
+    // Aquí se convierte el Entity en Dto
+    @Mapping(source = "titulo", target = "title")
+    @Mapping(source = "duracion", target = "duration")
+    @Mapping(source = "genero", target = "genre")
+    @Mapping(source = "fechaEstreno", target = "releaseDate")
+    @Mapping(source = "clasificacion", target = "rating")
+    MovieDto toDto(MovieEntity entity);
+    List<MovieDto> toDto(Iterable<MovieEntity> entities);
 }
